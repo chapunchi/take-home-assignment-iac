0ec4db706d6a2fbac0defd10b3221ee507acd87e
Chandira <chapunchi@gmail.com>
2025-10-02 12:53:57 +0530

    Accounts data script

--- modules/ddb/main.tf
+++ modules/ddb/main.tf
@@ -8,10 +8,11 @@ resource "aws_dynamodb_table_item" "table_item" {
   hash_key   = data.aws_dynamodb_table.accounts.hash_key
 
   item = jsonencode({
-    account_id      = { S = each.value.account_id }
-    current_balance = { N = each.value.current_balance }
-    first_name      = { S = each.value.first_name }
-    last_name       = { S = each.value.last_name }
-    daily_limit     = { N = each.value.daily_limit }
+    account_id             = { S = each.value.account_id }
+    current_balance        = { N = each.value.current_balance }
+    first_name             = { S = each.value.first_name }
+    last_name              = { S = each.value.last_name }
+    daily_limit            = { N = each.value.daily_limit }
+    daily_amount_withdrawn = { N = each.value.daily_amount_withdrawn }
   })
 }
