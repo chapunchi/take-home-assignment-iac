e00d62c0868d3fb592b855af6faab426706bd697
Chandira <chapunchi@gmail.com>
2025-10-02 12:46:15 +0530

    Accounts data script

--- modules/ddb/variables.tf
+++ modules/ddb/variables.tf
@@ -1,39 +1,44 @@
 variable "accounts" {
   default = [
     {
-      account_id      = "88888"
-      current_balance = "7500.25"
-      first_name      = "Devon"
-      last_name       = "Smith"
-      daily_limit     = "500"
+      account_id             = "88888"
+      current_balance        = 7500.25
+      first_name             = "Devon"
+      last_name              = "Smith"
+      daily_limit            = 500,
+      daily_amount_withdrawn = 100
     },
     {
-      account_id      = "12345"
-      current_balance = "1057.86"
-      first_name      = "john"
-      last_name       = "doe"
-      daily_limit     = "500"
+      account_id             = "12345"
+      current_balance        = 1057.86
+      first_name             = "john"
+      last_name              = "doe"
+      daily_limit            = 500,
+      daily_amount_withdrawn = 100
     },
     {
-      account_id      = "77777"
-      current_balance = "1500.78"
-      first_name      = "Alicia"
-      last_name       = "Howay"
-      daily_limit     = "500"
+      account_id             = "77777"
+      current_balance        = 1500.78
+      first_name             = "Alicia"
+      last_name              = "Howay"
+      daily_limit            = 500,
+      daily_amount_withdrawn = 100
     },
     {
-      account_id      = "55555"
-      current_balance = "6589.36"
-      first_name      = "Britany"
-      last_name       = "Smith"
-      daily_limit     = "500"
+      account_id             = "55555"
+      current_balance        = 6589.36
+      first_name             = "Britany"
+      last_name              = "Smith"
+      daily_limit            = 500,
+      daily_amount_withdrawn = 100
     },
     {
-      account_id      = "99999"
-      current_balance = "2399.22"
-      first_name      = "Jo"
-      last_name       = "Simmons"
-      daily_limit     = "500"
+      account_id             = "99999"
+      current_balance        = 2399.22
+      first_name             = "Jo"
+      last_name              = "Simmons"
+      daily_limit            = 500,
+      daily_amount_withdrawn = 100
     }
   ]
 }
