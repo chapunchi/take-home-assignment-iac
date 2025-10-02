a001a05cde6cf71169ca44ac123a842d3a63bb5a
Chandira <chapunchi@gmail.com>
2025-10-02 12:26:05 +0530

    Accounts data script

--- modules/ddb/main.tf
+++ modules/ddb/main.tf
@@ -2,18 +2,16 @@ data "aws_dynamodb_table" "accounts" {
   name = "Accounts"
 }
 
-# resource "aws_dynamodb_table_item" "table_item" {
-#   table_name = data.aws_dynamodb_table.accounts.name
-#   hash_key   = data.aws_dynamodb_table.accounts.hash_key
+resource "aws_dynamodb_table_item" "table_item" {
+  for_each   = { for acct in var.accounts : acct.account_id => acct }
+  table_name = data.aws_dynamodb_table.accounts.name
+  hash_key   = data.aws_dynamodb_table.accounts.hash_key
 
-#   item = <<ITEM
-# {
-#   "account_id": {"S": "8888888"},
-#   "current_balance": {"N": "1000"},
-#   "first_name": {"S": "fname"},
-#   "last_name": {"S": "lname"},
-#   "daily_limit": {"N": "500"},
-#   "daily_amount_withdrawn": {"N": "100"}
-# }
-# ITEM
-# }
+  item = jsonencode({
+    account_id      = { S = each.value.account_id }
+    current_balance = { N = each.value.current_balance }
+    first_name      = { S = each.value.first_name }
+    last_name       = { S = each.value.last_name }
+    daily_limit     = { N = each.value.daily_limit }
+  })
+}
