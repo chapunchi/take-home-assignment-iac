96a80d14097ca31d2f2746d99de730c91452d79b
Chandira <chapunchi@gmail.com>
2025-10-02 12:17:09 +0530

    Accounts table

--- modules/ddb/main.tf
+++ modules/ddb/main.tf
@@ -2,19 +2,18 @@ data "aws_dynamodb_table" "accounts" {
   name = "Accounts"
 }
 
-resource "aws_dynamodb_table_item" "table_item" {
-  table_name = data.aws_dynamodb_table.accounts.name
-  hash_key   = data.aws_dynamodb_table.accounts.hash_key
+# resource "aws_dynamodb_table_item" "table_item" {
+#   table_name = data.aws_dynamodb_table.accounts.name
+#   hash_key   = data.aws_dynamodb_table.accounts.hash_key
 
-  item = <<ITEM
-{
-  "account_id": {"S": "8888888"},
-  "current_balance": {"N": "1000"},
-  "first_name": {"S": "fname"},
-  "last_name": {"S": "lname"},
-  "daily_limit": {"N": "500"},
-  "withdraw_flag": {"S": "Y"},
-  "daily_amount_withdrawn": {"N": "100"}
-}
-ITEM
-}
+#   item = <<ITEM
+# {
+#   "account_id": {"S": "8888888"},
+#   "current_balance": {"N": "1000"},
+#   "first_name": {"S": "fname"},
+#   "last_name": {"S": "lname"},
+#   "daily_limit": {"N": "500"},
+#   "daily_amount_withdrawn": {"N": "100"}
+# }
+# ITEM
+# }
